1e4f6b97cf97bdb1f4da472974e838ac30aa98d5
Rod <rodrigo.fraga@gmail.com>
2019-02-17 23:41:44 +0000

    add build boost 1.69 script

---
+++ build_boost.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# download and build boost 1.6.9
+
+wget https://dl.bintray.com/boostorg/release/1.69.0/source/boost_1_69_0.zip
+
+unzip -qq boost_1_69_0.zip
+ren boost_1_69_0 boost
+
+cd boost
+bootstrap.bat
+b2.exe link=static runtime-link=static threading=multi --layout=versioned
+cd ..
+
