7879e0d2ed9447414ab93dfccee17cbb9b6cccbc
Rod <rodrigo.fraga@gmail.com>
2019-02-18 02:18:25 +0000

    trying out powershell script

--- build_boost.ps1
+++ build_boost.ps1
@@ -1,1 +1,12 @@
-Write-Host "hello from powershell"
+Write-Host "getting boost"
+wget --quiet https://dl.bintray.com/boostorg/release/1.69.0/source/boost_1_69_0.zip
+
+Write-Host "unzipping boost"
+unzip -qq boost_1_69_0.zip
+mv boost_1_69_0 boost
+
+cd boost
+
+Write-Host "Bootstrapping"
+
+bootstrap.bat
