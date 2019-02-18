d6325ceb37a1507a279a3f54615e0a56c36f3b24
Rod <rodrigo.fraga@gmail.com>
2019-02-18 00:16:47 +0000

    trying to execute build_boost

--- build_boost.sh
+++ build_boost.sh
@@ -4,11 +4,11 @@
 
 wget https://dl.bintray.com/boostorg/release/1.69.0/source/boost_1_69_0.zip
 
-unzip -qq boost_1_69_0.zip
-mv boost_1_69_0 boost
+#unzip -qq boost_1_69_0.zip
+#mv boost_1_69_0 boost
 
-cd boost
-bootstrap.bat
-b2.exe link=static runtime-link=static threading=multi --layout=versioned
-cd ..
+#cd boost
+#bootstrap.bat
+#b2.exe link=static runtime-link=static threading=multi --layout=versioned
+#cd ..
 
