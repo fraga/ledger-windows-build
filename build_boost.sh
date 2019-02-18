fe0482c0717617921b2a4f122306e254907a9b87
Rod <rodrigo.fraga@gmail.com>
2019-02-18 01:33:07 +0000

    some script logs

--- build_boost.sh
+++ build_boost.sh
@@ -4,9 +4,12 @@
 
 wget --quiet https://dl.bintray.com/boostorg/release/1.69.0/source/boost_1_69_0.zip
 
+echo "unzipping boost"
+
 unzip -qq boost_1_69_0.zip
 mv boost_1_69_0 boost
 
+echo "building boost"
 cd boost
 bootstrap.bat
 b2.exe link=static runtime-link=static threading=multi --layout=versioned
